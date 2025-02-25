8fc7c512e3ea897e9202cbcceb9ed6c1cce2c0ab
minhnhatcuteto1 <huynhdongnguyen240307@gmail.com>
2025-02-25 07:24:09 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  document.getElementByid(compiler).style.display="block"
+  let u = username.value
+  let p = pw.value 
+  if (u=="hutao" && p"123")
+  {
+   Window.open()
+  }
+}
